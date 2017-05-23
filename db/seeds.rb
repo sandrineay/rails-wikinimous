4608164311bc68f3ff00f42216c33bd2fe51d660
Sandrine Ayral <sandrine.ayral@gmail.com>
2017-05-23 15:32:14 +0100

    added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  article = Article.new(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraph(2, false, 4),
+  )
+  article.save
+end
